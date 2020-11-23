0d3b28d880882ea9dd1c25e083ac9219cdcd1dc9
Shirley Leu <shirley.leu@adevinta.com>
2020-11-23 22:37:42 +0100

    first version of quiz game

--- hack2/purple/main.go
+++ hack2/purple/main.go
@@ -1,29 +1,77 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
-	i "github.com/shirleyleu/interact"
+	"io"
+	"log"
+	"os"
+	"strings"
+
+	i "github.com/tockins/interact"
 )
 
-
 func main() {
-	firstQuestion:= "AWS"
+	in, err := os.Open("awsacronyms.csv")
+	if err != nil {
+		panic(err)
+	}
+	r := csv.NewReader(in)
 
-	i.Run(&i.Interact{
-		Questions: []*i.Question{
-			{
-				Quest: i.Quest{
-					Msg:      fmt.Sprintf("What does %s stand for?", firstQuestion),
-				},
-				Action: func(c i.Context) interface{} {
-					val, err := c.Ans().String()
-					if err != nil{
-						return err
-					}
-					fmt.Println(val)
-					return nil
-				},
-			},
+	var acronymsAndAnswers [][]string
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		acronymsAndAnswers = append(acronymsAndAnswers, record)
+	}
+
+	questionsAndResponses := map[string]string{}
+
+	for _, line := range acronymsAndAnswers {
+		questionsAndResponses[line[0]] = line[1]
+	}
+
+	var questions []*i.Question
+	for question, answer := range questionsAndResponses {
+		questions = append(questions, questionsBuilder(question, answer))
+	}
+
+	questionsFormat(questions)
+	// yay we are the friendliest bestest team
+	//  #ParticipationTrophy :)
+	// oops i've mucked the pointer still....
+	// * is a pointer
+	// & is the address it's about passing a specific shared value around instead of a copy but it's easy to mess up accidentally
+
+	// so use to Js where all that is handled for us, and only have to do scoping
+	// but go i feel is way more powerful
+}
+
+func questionsBuilder(question string, answer string) *i.Question {
+	return &i.Question{
+		Quest: i.Quest{
+			Msg: fmt.Sprintf("What does %s stand for?", question),
 		},
-	})
+		Action: func(c i.Context) interface{} {
+			val, err := c.Ans().String()
+			if err != nil {
+				return err
+			}
+			if strings.ToLower(val) != strings.ToLower(answer) {
+				fmt.Printf("NOPE! It was %s.\n", answer)
+				return nil
+			}
+
+			return nil
+		},
+	}
+}
+
+func questionsFormat(questions []*i.Question) {
+	i.Run(&i.Interact{Questions: questions})
 }
