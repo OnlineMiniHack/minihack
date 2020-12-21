8fe86c1c82af75f7a8a14567d05c4dd97cb5e745
romeo adjam <sodangbe@users.noreply.github.com>
2020-12-21 14:56:44 -0600

    finished

--- hack3/christmasCountdown/src/app/app-routing.module.ts
+++ hack3/christmasCountdown/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CountdownComponent } from './countdown/countdown.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',  component: CountdownComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
