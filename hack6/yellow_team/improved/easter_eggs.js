3252042e678c69a43e4bfbc89e052d939a6957b9
ljdyer <74581995+ljdyer@users.noreply.github.com>
2021-03-24 05:15:59 +0000

    Version 1.1
    Tidied up html, js, css and adjusted link formatting in "improved" folder. Original solution presented on the night moved to "original" folder.

---
+++ hack6/yellow_team/improved/easter_eggs.js
@@ -0,0 +1,89 @@
+// Easter egg 1: Append Easter Egg image to "pics" div
+
+function appendEggImage(src, width, height, alt){
+    var img = document.createElement("img");
+    img.src = "https://blog.creatopy.com/wp-content/uploads/2020/03/Funny-Camouflage-Easter-Egg-Hunt-Card.png";
+    var el = document.getElementById("pics");
+    pics.appendChild(img); 
+}
+
+// Easter egg 2: Make window move from side to side
+
+var myVar = 0;
+var d = 0;
+var x = 0;
+var s = 0;
+var s2 = 0;
+
+function tap() {
+    myVar = setInterval(tapStep, 10);
+    s = 10;
+    d = 1;
+    s2 = 10;
+}
+
+function tapStep() {
+    var el = document.getElementById('main');
+    el.style = "transform: translate(" + x + "px, 0);";
+    x = x + d;
+    s2 = s2 - 1;
+    if (s2 == 0) {
+        s = s - 1;
+        d = -d;
+        s2 = s;
+    }
+    if (s == 0) {
+        clearInterval(myVar);
+    }
+}
+
+// Easter egg 3: Make main div rotate 360 degrees
+
+function roll() {
+    var el = document.getElementById('main');
+    el.classList.add('roll');
+}
+
+// Easter egg 4: Make image fall
+
+function fall() {
+    var el = document.getElementById('fall');
+    el.classList.add('fall');
+}
+
+// Easter egg 5: Change background and text to random colours
+
+function colour() {
+    var el = document.body;
+    var red = 254 - (Math.random() * 50);
+    var green = 254 - (Math.random() * 50);
+    var blue = 254 - (Math.random() * 50);
+    el.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
+
+    red = Math.random() * 150;
+    green = Math.random() * 150;
+    blue = Math.random() * 150;
+    el.style.color = "rgb(" + red + "," + green + "," + blue + ")";
+}
+
+// Easter egg 6: Append easter icons to address bar
+
+var f = ['🥚🍫🐇🐤🙏'];
+
+function flashURL(){
+    location.hash = f[Math.floor((Date.now() / 100) % f.length)].repeat(20);
+    setTimeout(flashURL, 250);
+}
+
+// Easter egg 7: Say "Happy Easter to You!"
+
+function sayHappyEaster(){
+    speakText("Happy Easter to You!");
+}
+
+function speakText(textToSay) {
+    const synth = window.speechSynthesis;
+    const utterThis = new SpeechSynthesisUtterance(textToSay);
+    synth.speak(utterThis);
+}
+
