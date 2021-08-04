36e9f511ebbd424905f5aba356426a2d705e483b
bens-gitname <benegan435@gmail.com>
2021-08-04 19:56:12 +0100

    initial

---
+++ hack10/index.js
@@ -0,0 +1,13 @@
+//web page?
+
+//-- themes
+
+//  hungry caterpillar
+//  tutorial - how to something
+//  new junior dev job - tutorial
+//  //  build a table or something
+//  aircraft ?  balloon festival
+//  seasonal
+
+//--- interactions
+//  move balloon with cursor keys?
